3013e998018814f6340c6da3fcb348d0d23217f4
James Larisch <jamesfordummies@gmail.com>
2015-05-25 20:15:40 -0700

    cleanup, add player, asteroid, bullet type

--- Main.hs
+++ Main.hs
@@ -7,46 +7,66 @@ import Debug.Trace
 ship :: Form
 ship = filled white $ ngon 3 20.0
 
-topSpeed = 4.0
+friction = 0.95
 acceleration = 1.0
-decay = 0.1
+worldWidth = 800
+worldHeight = 600
+startBLife = 10
 
-mDecay :: Double -> Double
-mDecay time = topSpeed * ((1.0 - decay) ** time)
-
--- wow. unit circle comes in handy
-thrust :: Int -> Obj -> Obj
-thrust dy obj | dy >= 0 = obj
-thrust dy (Obj x y vx vy r) = Obj x y (vx + nx) (vy + ny) r
-      where (nx, ny) = (round $ acceleration * cos r, round $ acceleration * sin r)
-
-moveObj :: Obj -> Obj
-moveObj (Obj x y vx vy r) = Obj nx ny vx vy r
-          where nx = round $ 0.95 * (fromIntegral (x + vx))
-                ny = round $ 0.95 * (fromIntegral (y + vy))
-
-data Obj = Obj {
+data Player = Player {
   x :: Int,
-  y :: Int,
-  vx :: Int,
-  vy :: Int,
-  rotation :: Double
+    y :: Int,
+    vx :: Int,
+    vy :: Int,
+    rotation :: Double
+}
+
+data Asteroid = Asteroid {
+  ax :: Int,
+     ay :: Int,
+     avx :: Int,
+     avy :: Int
+}
+
+data Bullet = Bullet {
+  bx :: Int,
+  by :: Int,
+  life :: Int
 }
 
 data State = State {
-  player :: Obj,
-  asteroids :: [Obj]
+  player :: Player,
+  asteroids :: [Asteroid]
 }
+
+-- wow. unit circle comes in handy
+thrust :: Int -> Player -> Player
+thrust dy player | dy >= 0 = player
+thrust dy (Player x y vx vy r) = Player x y (vx + nx) (vy + ny) r
+      where (nx, ny) = (round $ acceleration * cos r, round $ acceleration * sin r)
+
+moveP :: Player -> Player
+moveP (Player x y vx vy r) = Player nx ny vx vy r
+          where nx = round $ friction * (fromIntegral (x + vx)) -- friction
+                ny = round $ friction * (fromIntegral (y + vy))
+
 
 step :: (Int, Int) -> State -> State
 -- step (dx, dy) (State (Obj x y vx vy) asteroids) | trace (show dx) False = undefined
-step (dx, dy) (State (Obj x y vx vy r) asteroids) = State player asteroids
-                    where newRotation = r + ((realToFrac dx) / 10.0)
-                          player = moveObj $ thrust dy $ Obj x y vx vy newRotation
+step (dx, dy) (State (Player x y vx vy r) asteroids) = 
+    State player asteroids
+    where newRotation = r + ((realToFrac dx) / 10.0)
+          player = moveP $ thrust dy $ Player x y vx vy newRotation
 
 render :: State -> Element
-render (State (Obj x y vx vy r) asteroids) =
-  centeredCollage 500 500 [move ((realToFrac x), (realToFrac y)) $ rotate r ship]
+render (State (Player x y vx vy r) asteroids) =
+  collage worldWidth worldHeight [move ((wrapX x), (wrapY y)) $ rotate r ship]
+
+wrapX :: Int -> Double
+wrapX x = realToFrac $ mod x worldWidth
+
+wrapY :: Int -> Double
+wrapY y = realToFrac $ mod y worldHeight
 
 sig :: Signal (Int, Int)
 sig = lift2 (\ x y -> x) Keyboard.arrows (Time.every Time.millisecond)
@@ -54,7 +74,7 @@ sig = lift2 (\ x y -> x) Keyboard.arrows (Time.every Time.millisecond)
 main :: IO ()
 main = run defaultConfig $ render <~ stepper
   where
-    defaultPlayer = Obj 100 100 0 0 0.0
-    defaultAsteroid = [Obj 20 20 5 5 0.0]
+    defaultPlayer = Player 100 100 0 0 0.0
+    defaultAsteroid = [Asteroid 20 20 5 5]
     state = State defaultPlayer defaultAsteroid
     stepper = foldp step state sig
